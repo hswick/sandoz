21752c74f548f1ba988f870fab2ae267b596a515
Yong Bakos <ybakos@humanoriented.com>
2016-02-29 18:49:04 -0600

    Add a playful 'new' command.

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,9 +4,14 @@ module Sandoz
 
   class Cli < Thor
 
-    desc "hello", "say hello"
+    desc "hello", "Say hello"
     def hello
       puts "Well, I met a girl called Sandoz\nAnd she taught me many, many things\nGood things, very good things, sweet things."
+    end
+
+    desc "new", "Take a hit and create a new sandoz project"
+    def new
+      `say Well, I met a girl called Sandoz`
     end
 
   end
